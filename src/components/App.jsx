769d21d96a5c98de2db09befb8d6fc887b0900cb
papajo <joshipv2@gmail.com>
2016-04-14 16:33:52 -0400

    setup webpack and react

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+Class App extends from Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <div>
+          This is my App 
+      </div>
+    )
+  }
+}
+
+export default App;
